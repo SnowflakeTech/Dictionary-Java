e09f22b9dd8e49a48c7c93609c23004315b25783
TomoriuAkarin <144638099+lequocanh10000@users.noreply.github.com>
2023-10-16 22:35:05 +0700

    Add files via upload

---
+++ Dictionary-Java/Dictionary.java
@@ -0,0 +1,6 @@
+import java.util.*;
+import java.io.*;
+
+public class Dictionary {
+    ArrayList<Word> words = new ArrayList<>();
+}
---
+++ Dictionary-Java/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+        dictionaryCommandline.dictionaryBasic();
+    }
+}
