83924825a2af4e865ade4dd2a9e2f24aa5c5a763
Phạm Tiến Sơn <22024531@vnu.edu.vn>
2023-10-27 21:40:44 +0700

    Thêm chức năng xoá

--- Main.java
+++ Main.java
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 package com.example.dictionary;
 import java.util.*;
-
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
         dictionaryCommandline.dictionaryBasic();
     }
-=======
-import java.util.*;
-
-import java.io.*;
-public class Main {
-    public static void main(String[] args) {
-        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
-        dictionaryCommandline.dictionaryBasic();
-    }
->>>>>>> 06c03390bbb7c5bdc3812b42f6eeeb457fc0f448
 }
